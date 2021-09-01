07a471510981939a4e6157c8c1cf826581667cea
Md Sulayman Hosen Abir <73769735+Sh75cr7@users.noreply.github.com>
2021-09-01 23:44:05 +0600

    bank with tailwiind

---
+++ js/login.js
@@ -0,0 +1,12 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+    // get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    // check email and password
+    if (userEmail == 'abirstu9@.com' && userPassword == '17502004537') {
+        window.location.href = 'banking.html';
+    }
+});
